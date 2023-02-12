5895152ba4fd7a5ae9c49b42d3cf4b4a6e4ceca7
MAD <wwwMADwww@users.noreply.github.com>
2023-02-12 03:11:00 +0300

    Program.cs cleanup, added dedicated dir for configuration code with examples new wav test files removed CommandLineParser changed target framework to NET 6

---
+++ FT2232ImageOutput/configs/ConfigExampleSvgShiftRegs.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Threading;
+using FT2232ImageOutput.FrameProcessors;
+using FT2232ImageOutput.HardwareOutput;
+using FT2232ImageOutput.MainProcessors;
+using FT2232ImageOutput.PathImages;
+using FT2232ImageOutput.PathImages.PathSources;
+using FT2232ImageOutput.PointBitMappers;
+using ManuPath.FillGenerators;
+using ManuPath.PrimitiveConverters;
+
+namespace FT2232ImageOutput.Configs
+{
+    static class ConfigExampleSvgShiftRegs
+    {
+        public static void Run()
+        {
+            uint baudrate = 500_000;
+
+            var filepath = @"..\..\..\samplefiles\svg\circle.svg";
+
+            var targetMaxValues8 = new ImageMaxValues() // XYZ 8 bit
+            {
+                MaxRGB = 255,
+                MinX = 0, MaxX = 255,
+                MinY = 0, MaxY = 255,
+                MinZ = 0, MaxZ = 255,
+            };
+            var targetMaxValues10 = new ImageMaxValues() // XY 10 bit, Z 4 bit
+            {
+                MaxRGB = 255,
+                MinX = 0, MaxX = 1023,
+                MinY = 0, MaxY = 1023,
+                MinZ = 0, MaxZ = 15,
+            };
+            var targetMaxValues = targetMaxValues10;
+
+
+            var oscilloscopeScreenSize = new Vector2(8 * 12, 8 * 12);
+
+            var pixelSize = new Vector2(
+                (targetMaxValues.MaxX - targetMaxValues.MinX) / oscilloscopeScreenSize.X,
+                (targetMaxValues.MaxY - targetMaxValues.MinY) / oscilloscopeScreenSize.Y
+                );
+            var pixelMin = Math.Min(pixelSize.X, pixelSize.Y);
+
+            var imageSource = new PathImageSource(
+                pathSource: new SvgFilePathSource(filepath),
+            
+                strokeConverter: new PrimitiveToEvenSegmentsConverter(pixelMin * 0.3f, pixelMin * 0.3f + 0.3f, false),
+                strokeBrightness: ColorChannel.Alpha,
+            
+                fillConverter: null,
+                fillGeneratorFactory: path => new IntervalDotsFillGenerator(
+                    path,
+                    new Vector2(1.2f, 1.2f) * pixelSize,
+                    new Vector2(1.1f, 1.1f) * pixelSize
+                    ),
+                fillIntensity : ColorChannel.Alpha,
+                fillBrightness: ColorChannel.Green,
+            
+                maxValues: targetMaxValues,
+                forceNotStreaming: true
+            );
+
+
+            var frameProcessors = new List<IFrameProcessor>() {
+               new AddBlankingPointsFrameProcessor(4, 4, true)
+            };
+             
+            var pointBitMapper = new ShiftRegisterPointBitMapper(
+                // mode: ShiftRegisterPointBitMapperMode.Mode_Sr8x3_XY8_Z8, // XYZ 8 bit
+                mode: ShiftRegisterPointBitMapperMode.Mode_Sr8x6_XY10_Z4, // XY 10 bit, Z 4 bit
+                invertZ: false, 
+                maxValues: targetMaxValues
+                );
+
+            var hardwareOutput = new FT2232HardwareOutput("A", baudrate, 10240);
+
+            var mainProcess = new MainProcessor(imageSource, frameProcessors, pointBitMapper, hardwareOutput, 0, true);
+
+            mainProcess.Start();
+
+            Thread.Sleep(Timeout.Infinite);
+        }
+
+
+
+
+    }
+}
---
+++ FT2232ImageOutput/configs/ConfigMatrix.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using FT2232ImageOutput.FrameProcessors;
+using FT2232ImageOutput.HardwareOutput;
+using FT2232ImageOutput.ImageSources;
+using FT2232ImageOutput.MainProcessors;
+using FT2232ImageOutput.PointBitMappers;
+
+namespace FT2232ImageOutput.Configs
+{
+    static class ConfigMatrix
+    {
+        public static void Run()
+        {
+            uint baudrate = 2_000_000;
+
+            var matrixSymbolsPath = @"..\..\..\samplefiles\matrixSymbols\";
+
+            var targetMaxValues = new ImageMaxValues() 
+            {
+                MaxRGB = 255,
+                MinX = 0, MaxX = 1023,
+                MinY = 0, MaxY = 1023,
+                MinZ = 0, MaxZ = 15,
+            };
+
+            var imageSource = CrateMatrix(
+                matrixSymbolsPath,
+                new MatrixImageSourceConfig()
+                {
+                    Width = 1024,
+                    Height = 1024,
+                    MaxRGB = 255,
+                    Cols = 28,
+                    Rows = 19,
+                    TrailLength = 25,
+                    TrailGenerationRate = 20,
+                    TrailChangeRateMin = 15,
+                    TrailChangeRateMax = 50,
+                    TrailChangeIntencity = 50
+                },
+                targetMaxValues
+            ); ;
+
+            var pointBitMapper = new ShiftRegisterPointBitMapper(
+                mode: ShiftRegisterPointBitMapperMode.Mode_Sr8x6_XY10_Z4,
+                invertZ: false,
+                maxValues: targetMaxValues
+                );
+
+            var frameProcessors = new List<IFrameProcessor>() { };
+
+            var hardwareOutput = new FT2232HardwareOutput("A", baudrate, 10240);
+
+            var mainProcess = new MainProcessor(imageSource, frameProcessors, pointBitMapper, hardwareOutput, 0, true);
+
+            mainProcess.Start();
+
+            Thread.Sleep(Timeout.Infinite);
+        }
+
+        static IImageSource CrateMatrix(string dir, MatrixImageSourceConfig config, ImageMaxValues maxValues)
+        {
+
+            List<Symbol> symbols = new List<Symbol>();
+
+
+            var blanker = new AddBlankingPointsFrameProcessor(10, 10, true);
+
+            foreach (var file in Directory.EnumerateFiles(dir, "*.ild"))
+            {
+
+                var imagesource = new IldaImageSource(Path.Combine(dir, file));
+
+                var scaler = new ScaleMaxValuesFrameProcessor(imagesource.MaxValues, maxValues);
+
+                var image = imagesource.GetFrames()
+                    .Where(f => f.Points.Any())
+                    .Select(f => scaler.Process(f))
+                    .Select(f => blanker.Process(f));
+
+                foreach (var frame in image)
+                {
+                    var s = new Symbol()
+                    {
+                        MaxValues = maxValues,
+                        Name = Path.GetFileNameWithoutExtension(file),
+                        Points = frame.Points.ToArray()
+                    };
+                    symbols.Add(s);
+                }
+
+            }
+
+            var imageSource = new MatrixImageSource(config, symbols);
+
+            return imageSource;
+        }
+
+
+    }
+}
