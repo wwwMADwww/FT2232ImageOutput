664ca349231d7843321a3cc59aa72c83909fe512
MAD <wwwMADwww@users.noreply.github.com>
2024-09-09 01:07:51 +0300

    added loading coordinates from text file

---
+++ FT2232ImageOutput/ImageSources/TextFileImageSource.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+using FT2232ImageOutput.Utils;
+
+namespace FT2232ImageOutput.ImageSources;
+
+
+public class TextFileImageSource : IImageSource
+{
+    private readonly string _filePath;
+    private List<ImageFrame> _frames;
+
+    public TextFileImageSource(string filePath, ImageMaxValues maxValues)
+    {            
+        _filePath = Path.GetFullPath(filePath);
+
+        if (!File.Exists(_filePath))
+            throw new FileNotFoundException($"File '{_filePath}' does not exists.");
+
+        if (Directory.Exists(_filePath))
+            throw new ArgumentException($"Path '{_filePath}' is a directory.");
+
+        MaxValues = maxValues;
+    }
+
+
+    public ImageType ImageType => ImageType.Vector;
+
+    public bool Streaming => false;
+
+    public ImageMaxValues MaxValues { get; set; }
+
+    public void Init()
+    { 
+        // col delimiter \t
+        // row delimiter \n or \r\n
+
+        var coordList = new List<Vector2>();
+
+        using (var sr = new StreamReader(new FileStream(_filePath, FileMode.Open, FileAccess.Read)))
+        {
+            while (true)
+            {
+                var line = sr.ReadLine();
+
+                if (line == null) break;
+
+                var coords = line.Split('\t');
+
+                var x = float.Parse(coords[0]);
+                var y = float.Parse(coords[1]);
+
+                coordList.Add(new Vector2(x, y));
+            }
+        }
+
+        _frames = new List<ImageFrame>();
+
+        var framePoints = new List<ImagePoint>();
+
+        var minX = coordList.MinBy(v => v.X).X;
+        var maxX = coordList.MaxBy(v => v.X).X;
+
+        var minY = coordList.MinBy(v => v.Y).Y;
+        var maxY = coordList.MaxBy(v => v.Y).Y;
+
+        var koeff = MathUtils.AspectRatio(maxX - minX, maxY - minY);
+
+        foreach (var coord in coordList)
+        { 
+            var point = new ImagePoint()
+            {
+                X = (int)MathUtils.ConvertRange(minX, maxX, MaxValues.MinX, MaxValues.MaxX * koeff.x, coord.X),
+                Y = (int)MathUtils.ConvertRange(minY, maxY, MaxValues.MinY, MaxValues.MaxY * koeff.y, coord.Y),
+                Z = MaxValues.MaxZ,
+
+                R = MaxValues.MaxRGB,
+                G = MaxValues.MaxRGB,
+                B = MaxValues.MaxRGB,
+
+                Blanking = false
+            };
+
+            framePoints.Add(point);
+        }
+
+        _frames.Add(new ImageFrame()
+        {
+            Duration = -1,
+            Number = 1,
+            Points = framePoints.ToArray()
+        });
+        
+    }
+
+    public IEnumerable<ImageFrame> GetFrames()
+    {
+        return _frames;
+    }
+
+}
---
+++ FT2232ImageOutput/configs/ConfigTextFileReg573.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Numerics;
+using System.Threading;
+using FT2232ImageOutput.FrameProcessors;
+using FT2232ImageOutput.HardwareOutput;
+using FT2232ImageOutput.MainProcessors;
+using FT2232ImageOutput.PointBitMappers;
+using FT2232ImageOutput.ImageSources;
+
+namespace FT2232ImageOutput.Configs;
+
+static class ConfigTextFileReg573
+{
+    public static void Run()
+    {
+        // uint baudrate = 12_000_000; // = 20 MBytes/s = 6.(6) MPoints/s
+
+        uint baudrate = 1_000_000;
+
+        var filepath = @"..\..\..\samplefiles\etc\konata coords.csv";
+
+        var targetMaxValuesXY10Z4 = new ImageMaxValues() // XY 10 bit Z 4 bit
+        {
+            MaxRGB = 31,
+            MinX = 0, MaxX = 1023,
+            MinY = 0, MaxY = 1023,
+            MinZ = 0, MaxZ = 31,
+        };
+        var targetMaxValues = targetMaxValuesXY10Z4;
+
+        var oscilloscopeScreenSize = new Vector2(8 * 12, 8 * 12);
+
+        var pixelSize = new Vector2(
+            targetMaxValues.Width / oscilloscopeScreenSize.X,
+            targetMaxValues.Height / oscilloscopeScreenSize.Y);
+
+        var pixelMin = Math.Min(pixelSize.X, pixelSize.Y);
+
+        var imageSource = new TextFileImageSource(filepath, targetMaxValues);
+        imageSource.Init();
+
+        var frameProcessors = new List<IFrameProcessor>() {
+               new AddBlankingPointsFrameProcessor(4, 4, true)
+            };
+
+        var pointBitMapper = new Reg573PointBitMapper(
+            invertZ: true,
+            maxValues: targetMaxValues);
+
+        // see console output for locationId
+        var hardwareOutput = new FT2232HardwareOutput(locationId: 37, baudrate, bufferSize: 100_000);
+
+        var mainProcess = new MainProcessor(imageSource, frameProcessors, pointBitMapper, hardwareOutput);
+
+        mainProcess.Start();
+
+        Thread.Sleep(Timeout.Infinite);
+    }
+
+
+}
